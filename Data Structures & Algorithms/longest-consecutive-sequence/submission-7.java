1abe877eafc5d08928fe87bf497b507c9f1d6784
Nawinkaarthik N <nawinkaarthikn07@gmail.com>
2026-09-21 19:57:12 +0530

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n,c=0;
+        if(nums.length==0){
+            return c;
+        }
+        Arrays.sort(nums);
+        for(int i=0,j=1;j<nums.length;i++,j++){
+            int a=nums[i];
+            int b=nums[j];
+            if(+(b-a)==1){
+                c+=1;
+            }
+        }
+        return c+1;
+    }
+}
